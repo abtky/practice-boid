27cd0e7805fded2a82117dd7769f125f4c1d7d34
Takuya Abe <abe_takuya@cocone.co.jp>
2019-09-02 20:32:21 +0900

    feat: Vector2Dクラスを実装

--- assets/ts/Vector2D.ts
+++ assets/ts/Vector2D.ts
@@ -57,6 +57,9 @@ export class Vector2D {
         this.y = Math.sin(value) * length;
     }
 
+    /**
+     * ベクトルの正規化（lengthを1にする）
+     */
     normalize(): Vector2D {
         if(this.length === 0) {
             // 0ベクトルの場合は(1,0)のベクトルにする
@@ -68,6 +71,122 @@ export class Vector2D {
         this.x /= length;
         this.y /= length;
         return this;
+    }
+
+    /**
+     * ベクトルの大きさを指定した値以下に収める
+     * @param max
+     */
+    truncate(max: number): Vector2D {
+        this.length = Math.min(max, this.length);
+        return this;
+    }
+
+    /**
+     * ベクトルの向きを反転する
+     */
+    reverse(): Vector2D {
+        this.x = -this.x;
+        this.y = -this.y;
+        return this;
+    }
+
+    /**
+     * このベクトルが正規化されていれば真
+     */
+    get isNormalized(): boolean {
+        return this.length === 1.0;
+    }
+
+    /**
+     * このベクトルと引数で与えられたベクトルの内積を計算する
+     * @param v2
+     */
+    dotProduct(v2: Vector2D): number {
+        return this.x * v2.x + this.y * v2.y;
+    }
+
+    /**
+     * ２つのベクトルの角度を計算する
+     */
+    static angleBetween(v1: Vector2D, v2: Vector2D): number {
+        if(!v1.isNormalized) {
+            v1 = v1.clone().normalize();
+        }
+        if(!v2.isNormalized) {
+            v2 = v2.clone().normalize();
+        }
+        return Math.acos(v1.dotProduct(v2));
+    }
+
+    /**
+     * このベクトルに垂直なベクトルを返す
+     */
+    get perpendicular() {
+        return new Vector2D(-this.y, this.x);
+    }
+
+    /**
+     * 与えられたベクトルがこのベクトルの右側にあるか判定
+     * @param v2
+     * @return {number} 左側なら−１、右側なら１
+     */
+    sign(v2: Vector2D): number {
+        return this.perpendicular.dotProduct(v2) < 0 ? -1 : 1;
+    }
+
+    distSQ(v2: Vector2D): number {
+        const dx: number = v2.x - this.x;
+        const dy: number = v2.y - this.y;
+        return dx * dx + dy * dy;
+    }
+
+    /**
+     * このベクトルと与えられたベクトルの距離を計算する
+     * @param v2
+     */
+    distance(v2: Vector2D): number {
+        return Math.sqrt(this.distSQ(v2));
+    }
+
+    /**
+     * このベクトルと与えられたベクトルを足した結果を取得
+     * @param v2
+     */
+    add(v2: Vector2D): Vector2D {
+        return new Vector2D(this.x + v2.x, this.y + v2.y);
+    }
+
+    /**
+     * このベクトルから与えられたベクトルを引いた結果を取得
+     * @param v2
+     */
+    subtract(v2: Vector2D): Vector2D {
+        return new Vector2D(this.x - v2.x, this.y - v2.y);
+    }
+
+    /**
+     * このベクトルを与えられた数値で掛けた結果を取得
+     * @param value
+     */
+    mutiply(value: number): Vector2D {
+        return new Vector2D(this.x - value, this.y - value);
+    }
+
+    /**
+     * このベクトルを与えられた数値で割った結果を取得
+     * @param value
+     */
+    divide(value: number): Vector2D {
+        return new Vector2D(this.x / value, this.y / value);
+    }
+
+    /**
+     * このベクトルと与えられたベクトルが等しければ真
+     * @param v2
+     */
+    isEquals(v2: Vector2D): boolean {
+        return this.x === v2.x && this.y === v2.y;
     }
 }
 
--- test/Vector2D.spec.ts
+++ test/Vector2D.spec.ts
@@ -6,4 +6,18 @@ describe('Vector2D', () => {
         instance.normalize();
         expect(instance.length).toBe(1);
     });
+    it('ベクトルの大きさを丸める', () => {
+        const instance = new Vector2D(20, 30);
+        instance.truncate(10);
+        expect(instance.length).toBe(10);
+    });
+    it('vector2d.sign', () => {
+        const instance = new Vector2D(20, 30);
+        const v2 = new Vector2D(20, 31);
+        const v3 = new Vector2D(20, 29);
+        console.log('dotProduct v2: ', instance.perpendicular.dotProduct(v2));
+        console.log('dotProduct v3: ', instance.perpendicular.dotProduct(v3));
+        expect(instance.sign(v2)).toBe(1);
+        expect(instance.sign(v3)).toBe(-1);
+    });
 });
